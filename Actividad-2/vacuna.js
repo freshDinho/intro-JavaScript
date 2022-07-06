6ed028ec7761a12a1725625b1bb22f390effcb76
Andrés Villa <sloweight39729@gmail.com>
2022-07-05 23:37:26 -0500

    Actividad-2 v3

--- Actividad-2/vacuna.js
+++ Actividad-2/vacuna.js
@@ -1,8 +1,9 @@
 let añoActual = 2022; //agregar funcion date para el añoActual.
 let añoNacimiento = 1990;
 let edad = añoActual - añoNacimiento;
-// let edad = 29;
-let embarazada = true;
+console.log("Edad:" + edad);
+// let edad = 32;
+let embarazada = false;
 let semanaGestacion = 10;
 let residenciaNorte = true; // agregar array.
 let vacuna = false;
@@ -15,12 +16,12 @@ if (
 ) {
   vacuna = true;
   console.log("Puedes Vacunarte, grupo: Embarazadas");
-} else if (edad >= 18 && embarazada == false && residenciaNorte == true) {
-  vacuna = true;
-  console.log("Puedes Vacunarte, grupo: Jovenes");
 } else if (edad >= 30 && embarazada == false && residenciaNorte == true) {
   vacuna = true;
   console.log("Puedes Vacunarte, grupo: Adultos");
+} else if (edad >= 18 && embarazada == false && residenciaNorte == true) {
+  vacuna = true;
+  console.log("Puedes Vacunarte, grupo: Jovenes");
 } else {
   console.log("No Puedes Vacunarte");
 }
