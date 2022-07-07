f6b03dd55b34094035effc5fef1969597d80a908
Andrés Villa <sloweight39729@gmail.com>
2022-07-07 13:34:08 -0500

    Actividad-3 v2 cambio en ejercicio 2 y 7 mejora

--- Actividad-3/parte1/Ejercicio2.js
+++ Actividad-3/parte1/Ejercicio2.js
@@ -2,27 +2,26 @@ let jugador1, jugador2;
 
 jugador1 = prompt("Elije piedra,papel o Tijera" + jugador1);
 jugador2 = prompt("Elije piedra,papel o Tijera" + jugador2);
-//if-piedra
-if (jugador1 == "piedra" && jugador2 == "papel") {
-  console.log("Ganador: jugador1 ");
-} else if (jugador1 == "piedra" && jugador2 == "piedra") {
-  console.log("Empate: ");
-} else if (jugador1 == "piedra" && jugador2 == "tijera") {
-  console.log("Ganador: jugador1 ");
-}
-//if-papel
-if (jugador1 == "papel" && jugador2 == "papel") {
-  console.log("Empate: ");
-} else if (jugador1 == "papel" && jugador2 == "piedra") {
-  console.log("Ganador: jugador2");
-} else if (jugador1 == "papel" && jugador2 == "tijera") {
-  console.log("Ganador: jugador2");
-}
-//if-tijera
-if (jugador1 == "tijera" && jugador2 == "papel") {
-  console.log("Ganador: jugador1");
-} else if (jugador1 == "tijera" && jugador2 == "piedra") {
-  console.log("Ganador: jugador2");
-} else if (jugador1 == "tijera" && jugador2 == "tijera") {
-  console.log("Empate: ");
+
+if (jugador1 == jugador2) {
+  console.log("Empate!");
+} else {
+  //if-piedra
+  if (jugador1 == "piedra" && jugador2 == "papel") {
+    console.log("Ganador: jugador1 ");
+  } else if (jugador1 == "piedra" && jugador2 == "tijera") {
+    console.log("Ganador: jugador1 ");
+  }
+  //if-papel
+  if (jugador1 == "papel" && jugador2 == "piedra") {
+    console.log("Ganador: jugador2");
+  } else if (jugador1 == "papel" && jugador2 == "tijera") {
+    console.log("Ganador: jugador2");
+  }
+  //if-tijera
+  if (jugador1 == "tijera" && jugador2 == "papel") {
+    console.log("Ganador: jugador1");
+  } else if (jugador1 == "tijera" && jugador2 == "piedra") {
+    console.log("Ganador: jugador2");
+  }
 }
--- Actividad-3/parte2/Ejericicio7.js
+++ Actividad-3/parte2/Ejericicio7.js
@@ -1,21 +1,65 @@
-let num, num2, num3;
-
-num = Number(prompt("Dame un numero"));
-num2 = Number(prompt("Dame otro numero"));
-num3 = Number(prompt("Dame otro numero"));
-
-if (num > num2 && num > num3) {
-  console.log(num + "es el mayor");
-} else if (num2 > num && num2 > num3) {
-  console.log(num2 + "es el mayor");
-} else if (num3 > num && num3 > num2) {
-  console.log(num3 + "es el mayor");
-} else if (num == num2 && num2 == num3) {
-  console.log("Todos los numeros son iguales en:" + num3);
-} else if (num == num2) {
-  console.log(num + "es igual a" + num2);
-} else if (num2 == num3) {
-  console.log(num2 + "es igual a" + num3);
-} else if (num3 == num) {
-  console.log(num3 + "es igual a" + num);
+let mayor = 0;
+num1 = Number(prompt("Primer Numero"));
+num2 = Number(prompt("Segundo Numero"));
+num3 = Number(prompt("Tercero Numero"));
+// let num1 = 2;
+// let num2 = 1;
+// let num3 = 2;
+if (num1 > num2 && num1 > num3) {
+  mayor = num1;
+  if (num2 > num3) {
+    console.log(`${mayor} > ${num2} > ${num3}`);
+  } else if (num2 == num3) {
+    console.log(`${mayor} > (${num2} == ${num3})`);
+  } else {
+    console.log(`${mayor} > ${num3} > ${num2}`);
+  }
+} else if (num2 > num1 && num2 > num3) {
+  mayor = num2;
+  if (num1 > num3) {
+    console.log(`${mayor} > ${num1} > ${num3}`);
+  } else if (num1 == num3) {
+    console.log(`${mayor} > (${num1} == ${num3})`);
+  } else {
+    console.log(`${mayor} > ${num3} > ${num1}`);
+  }
+} else if (num3 > num1 && num3 > num2) {
+  mayor = num3;
+  if (num1 > num2) {
+    console.log(`${mayor} > ${num1} > ${num2}`);
+  } else if (num1 == num2) {
+    console.log(`${mayor} > (${num1} == ${num2})`);
+  } else {
+    console.log(`${mayor} > ${num2} > ${num1}`);
+  }
+} else if (num1 > num2 && num1 == num3) {
+  mayor = num1;
+  console.log(`${mayor} se repite y ${mayor} > ${num2}`);
+} else if (num2 > num3 && num2 == num1) {
+  mayor = num2;
+  console.log(`${mayor} se repite y ${mayor} > ${num3}`);
+} else if (num3 > num1 && num2 == num3) {
+  mayor = num3;
+  console.log(`${mayor} se repite y ${mayor} > ${num1}`);
+} else if (num1 == num2 && num2 == num3) {
+  mayor = num1;
+  console.log(`Todos los numeros son iguales y son ${mayor}`);
+} else {
+  console.log("Error:");
 }
+
+/*Casos:
+----->No se repite
+1-2-3
+1-3-2
+2-1-3
+2-3-1
+3-1-2
+3-2-1
+----->Se repite
+1-2-2
+2-2-1
+2-1-2
+----->Todo se repite
+1-1-1
+*/
