de2ce3ad0b03ffbf4cb32abfac53366675d26598
Andrés Villa <sloweight39729@gmail.com>
2022-07-19 17:51:56 -0500

    Actividad-8 v2

--- Actividad-8/Eje9.js
+++ Actividad-8/Eje9.js
@@ -1,28 +1,75 @@
 /*
-Create an object that will be filled only by a function inside of it, and not from someone outside of it's function.. example:
+Create an object that will be filled only by a function inside of it, and not from one outside of its function.. example:
 
  let myObject = {
     myFunction: function ()... // This function is supposed to fill this object
  }
 
-myObject.myNewProp = "Hello world" // This should throw an exception or error because we don't want this object to be    filled from outside...
-It supposed to be filled using it's own function
-Add any type of element on the object.. arrays, objects, int, strings, etc
+myObject.myNewProp = "Hello world" // This should throw an exception or error because we don't want this object to be filled from outside...
+It's supposed to be filled using its own function
+Add any type of element on the object.. arrays, objects, int, strings, etc.
 */
 
+// POSIBLE SOLUCION 1 ¯\_(ツ)_/¯
+
 let myObject = {
-  add: function () {
-    myObject.ImAnString = "I can be added";
-    myObject.ImAnInt = 25;
-    myObject.ImAnArray = [1, "can", "be", "added"];
-    myObject.ImAnObject = {
-      Name: "I can be added",
-      age: 2,
-    };
+  add: function (string, int, array, object) {
+    myObject.ImAnString = string;
+    myObject.ImAnInt = int;
+    myObject.ImAnArray = array;
+    myObject.ImAnObject = object;
+    Object.freeze(myObject);
   },
 };
-myObject.add();
-Object.freeze(myObject);
-
+myObject.add("I can be added", 25, [1, "can", "be", "added"], {
+  Name: "I can be added",
+  age: 2,
+});
 myObject.myNewProp = "I can never be added";
 console.log(myObject);
+
+// POSIBLE SOLUCION 2 ¯\_(ツ)_/¯
+
+let myObject2 = {
+  add: function (string, int, array, object) {
+    Object.defineProperties(myObject2, {
+      Name: {
+        value: string,
+        configurable: false,
+        writable: false,
+      },
+      Age: {
+        value: int,
+        configurable: false,
+        writable: false,
+      },
+      Colors: {
+        value: array,
+        configurable: false,
+        writable: false,
+      },
+      Car: {
+        value: object,
+        configurable: false,
+        writable: false,
+      },
+    });
+    Object.freeze(myObject2);
+  },
+};
+
+myObject2.add("I'm a String", 20, ["red", "black", "white"], {
+  type: "Fiat",
+  model: 500,
+  color: "white",
+});
+myObject2.Pet = "dog"; // I never be added
+myObject2.Name = "Jhon"; // I never be added
+
+console.log(
+  myObject2.Name,
+  myObject2.Age,
+  myObject2.Colors,
+  myObject2.Car,
+  myObject2.Pet
+);
